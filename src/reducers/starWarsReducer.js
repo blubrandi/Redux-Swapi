2e119b6744f237ab11388ec5652f87a42c27bb46
blubrandi <blubrandi0@gmail.com>
2019-04-10 15:16:07 -0600

    Added dependencies on index.js, completed initial state on starWarsReducer.js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,11 @@
 import /* we need our action types here*/ "../actions";
+
+
 const initialState = {
-  characters: []
+  characters: [],
+  error: '',
+  isFetching: false
+
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
